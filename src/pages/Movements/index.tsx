be4ea22e960c4e42e6b7535b05c7d0ae0bfeac8e
jduran <jduran@gen-it.com.ar>
2022-07-17 13:54:42 -0300

    feat(movements): added flex styles

--- src/pages/Movements/index.tsx
+++ src/pages/Movements/index.tsx
@@ -1,7 +1,19 @@
+import "./movements.css";
+
 const Movements = () => {
    return (
-      <div>
+      <div className="movement-page">
          <h1>Movements</h1>
+
+         <div className="movement-content">
+            <div className="movement-content-table">
+               Tabla de movimientos
+            </div>
+
+            <div className="movement-content-form">
+               Formulario de movimientos
+            </div>
+         </div>
       </div>
    );
 }
